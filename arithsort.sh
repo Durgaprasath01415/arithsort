90c4013baf652d68b3c07fc988c52df235dff41a
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-03 22:25:26 +0530

    [Prasath] added the compute4 code

--- arithsort.sh
+++ arithsort.sh
@@ -5,3 +5,4 @@ read -p "enter the number c=" number3
 compute1=$(( number1+number2*number3 ))
 compute2=$(( number1*number2+number3 ))
 compute3=$(( number3+number1/number2 ))
+compute4=$(( number1%number2+number3 ))
