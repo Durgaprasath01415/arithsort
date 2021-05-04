32027cf7119fd043962850e53b41b2c5b3e6ec76
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-04 09:51:45 +0530

    [prasath] read the value from dictionary to array

--- arithsort.sh
+++ arithsort.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 declare -A dictionary
+declare -a array
 read -p "enter the number a=" number1
 read -p "enter the number b=" number2
 read -p "enter the number c=" number3
@@ -10,5 +11,11 @@ compute4=$(( number1%number2+number3 ))
 dictionary=(["1"]=$compute1 ["2"]=$compute2 ["3"]=$compute3 ["4"]=$compute4)
 for dict in ${dictionary[@]}
 do
-	echo $dict
+	echo "DICtIONARY="$dict
+done
+echo
+array=( ${dictionary[1]} ${dictionary[2]} ${dictionary[3]} ${dictionary[4]} )
+for arr in ${array[@]}
+do
+	echo "ARRAY="$arr
 done
